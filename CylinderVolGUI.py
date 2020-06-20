e639f8d61ba805cfb4972a179d4247ade21645a1
jwmasters <johnwmastersjr@gmail.com>
2020-06-19 20:56:56 -0700

    GUI Cylinder Vol tutorial

--- CylinderVolGUI.py
+++ CylinderVolGUI.py
@@ -1,4 +1,19 @@
 import tkinter as tk
+import math
+
+def submit():
+
+    print("Submit pressed!")
+    rad = float(entr.get())
+    hei = float(enth.get())
+
+    v = math.pi*rad*rad*hei
+    v = round(v, 3)
+
+    output.config(state="normal")
+    output.insert(tk.INSERT, v)
+    output.config(state="disabled")
+
 
 root = tk.Tk()
 root.title("Volume of a Cylinder")
@@ -15,11 +30,13 @@ labh.pack()
 enth = tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text="Submit")
+btn = tk.Button(root, text="Submit", command=submit)
 btn.pack()
 
 output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
+output.config(state="disabled")
 output.pack()
+
 #https://www.youtube.com/watch?v=d4k8-z_ZKfE&list=PLXPfOoCLXkKahgyuQZpn8VOCDBJaGFmB8&index=28
 #Paul Miskew 
 
